505470e81c780d63003be442949dd333e0deee0c
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-22 20:45:23 +0200

    change model 2

--- api/model.js
+++ api/model.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
 const FairyTaleSchema = new mongoose.Schema({
+  audioUrl: String,
+  createTime: Number,
   id: Number,
-  name: String
+  imageUrl: String,
+  lullaby:  Boolean,
+  name: String,
+  text: String,
+  updateTime: Number,
+  updated: Boolean
 });
 
 
