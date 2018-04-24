cfd82705b76ab1255035268c48c2cd0c7c70a8ee
Vitalik7 <lavrenko.vitalik@yandex.ua>
2018-04-24 13:38:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const host = 'localhost';
 module.exports = {
     port: process.env.port || process.env.PORT || '8080',
     // mongoUrl: process.env.MONGODB_URI || `mongodb://js7:111@ds119969.mlab.com:19969/tale` // Sergii db v2
-     mongoUrl: process.env.MONGODB_URI || `mongodb://admin:123@ds215759.mlab.com:15759/fairy-tales`  // Vitalik db
+     mongoUrl: process.env.MONGODB_URI || `mongodb://test:123@ds215759.mlab.com:15759/fairy-tales`  // Vitalik db
 
     };
